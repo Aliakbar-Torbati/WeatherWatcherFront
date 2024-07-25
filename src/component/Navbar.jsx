5e7e49f877ab7bad43378263c82778ecd0112db6
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-07-25 18:24:49 +0200

    dropdown window in navbar added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./NavbarStyle.scss";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useAuthen } from "../context/AuthenContex";
-
+import { TbRuler } from "react-icons/tb";
+import { auth } from "../firebaseConfig";
 
 const Navbar = () => {
-  const {uuser, setUuser } = useAuthen();
+  const { uuser, setUuser } = useAuthen();
+  const dropdownRef = useRef(null);
+
   // make a responsive and collapsible navbar
   const [clickNav, setClickNav] = useState(false);
-    // State to manage the dropdown visibility
-    const [showDropdown, setShowDropdown] = useState(false);
-    const toggleDropdown = () => {
-      setShowDropdown(!showDropdown);
-    };
-    
   const handleNavbar = () => {
     setClickNav(!clickNav);
   };
+  // State to manage the dropdown visibility
+  const [showDropdown, setShowDropdown] = useState(false);
+  const toggleDropdown = (event) => {
+    event.stopPropagation();
+    setShowDropdown(!showDropdown);
+  };
+
   // changing the color of navbar with scrolling
   const [navbarColor, setNavbarColor] = useState(false);
   const changeNavbarColor = () => {
@@ -27,6 +31,32 @@ const Navbar = () => {
     }
   };
   window.addEventListener("scroll", changeNavbarColor);
+
+  // Handle click outside to close the dropdown window
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  //log out function
+  const handleLogOut = async () => {
+    try {
+      await auth.signOut();
+      console.log("user logged out successfully");
+      nav("/");
+    } catch (error) {
+      console.log("error while logging out");
+    }
+  };
+
   return (
     <div className={navbarColor ? " header header-bg" : "header"}>
       <Link to={"/"}>
@@ -39,10 +69,29 @@ const Navbar = () => {
         <li>
           <Link to="/about">About</Link>
         </li>
+
         <li>
-          <Link to={uuser ? "/profile" : "/login"}>
+          {/* <Link to={uuser ? "/profile" : "/login"}>
               <FaRegUserCircle size={25} style={{ color: "#fff" }}/>
-          </Link>
+          </Link> */}
+           <div className="user-icon-wrapper" ref={dropdownRef}> 
+             <FaRegUserCircle size={25} style={{ color: "#fff" }} onClick={toggleDropdown} />
+             {showDropdown && (
+              <div className="dropdown-menu1">
+                {uuser? 
+                <div>
+                  <Link to="/profile"><li>My Account</li></Link>
+                  <button onClick={handleLogOut}>Log Out</button>
+                </div>
+                :
+                <div>
+                  <Link to="/login"><li>Log In</li></Link>
+                  <Link to="/signup"><li>Sign Up</li></Link>
+                </div>
+              }
+              </div>
+            )} 
+           </div>
         </li>
       </ul>
       <div className="hamburgerIcon">

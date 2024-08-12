bd33a9af54a173d955cd725de244d6c6756b006b
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-08-12 11:38:23 +0200

    vite setting changed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: true, // Enable source maps
     outDir: 'dist',
     rollupOptions: {
       input: {
